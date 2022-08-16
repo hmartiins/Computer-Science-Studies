6c321776df22258c981ccaadebe04179fc77a28c
hmartins <hmartins224@gmail.com>
2022-08-16 11:33:47 -0300

    (aa): refactoring the function to return the largest number of the vector and the number of comparisons by the pointer

--- 2stSemester/AnaliseDeAlgoritmo/Aula1Comparacao.c
+++ 2stSemester/AnaliseDeAlgoritmo/Aula1Comparacao.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 
-int numeroDeComparacoes(int[], int);
+int retornaMaiorNumero(int[], int *);
 
 int main()
 {
-  int i, vetor[6];
+  int i, numeroComparacoes = 6, vetor[numeroComparacoes];
 
   for (i = 0; i <= 5; i++)
   {
     scanf("%d", &vetor[i]);
   }
 
-  printf("\n\n%d", numeroDeComparacoes(vetor, 6));
+  printf("\nMaior numero: %d\n", retornaMaiorNumero(vetor, &numeroComparacoes));
+  printf("Numero de comparacoes: %d\n", numeroComparacoes);
 
   return 0;
 }
 
-int numeroDeComparacoes(int a[6], int n)
+int retornaMaiorNumero(int a[6], int *n)
 {
   int i, numeroDeComparacoes = 0;
   int x = a[0];
 
-  for (i = 1; i < n; i++)
+  for (i = 1; i < *n; i++)
   {
-    if (a[i] < x)
+    if (a[i] > x)
     {
       x = a[i];
     }
     numeroDeComparacoes = numeroDeComparacoes + 1;
   }
 
-  return numeroDeComparacoes;
+  *n = numeroDeComparacoes;
+
+  return x;
 }
